a85a3cbb4c1f0866d79347992bddf5bcc196d59c
Michael Jagiello <michaeljr623@hotmail.com>
2025-04-16 20:52:49 -0400

    color works

--- src/window.cpp
+++ src/window.cpp
@@ -59,52 +59,32 @@ int Window::SetupOpenGL() {
     // tell opengl window size
     //SDL_GetWindowSize(_window, &w, &h);
     glViewport(0, 0, _horRes, _vertRes);
-    glClearColor(0.0f, 0.5f, 1.0f, 0.0f);
 
-    // get into the opengl stuff
-
-    // vertex shader for compilation
-    const char* vertexShaderSource = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";//*/
-
-    /*constexpr auto vertexShaderSource = R"(
+    constexpr auto vertexShaderSource = R"(
         #version 330 core
+
         layout (location = 0) in vec3 aPos;
         layout (location = 1) in vec3 aColor;
-
-        uniform mat4 modelMatrix;
 
         out vec3 vertexColor;
 
         void main() {
-            gl_Position = modelMatrix * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+            gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
             vertexColor = aColor;
         }
-    )";//*/
-
-    // fragment shader for compilation
+    )";
+    
     constexpr auto fragmentShaderSource = R"(
         #version 330 core
-        out vec4 FragColor;
-        void main() {
-           FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-        };
-    )";//*/
-    
-    /*constexpr auto fragmentShaderSource = R"(
-        #version 330 core
-        out vec4 FragColor;
 
         in vec3 vertexColor;
 
+        out vec4 FragColor;
+
         void main() {
-            FragColor = vec4(vertexColor, 1.0);
+            FragColor = vec4(vertexColor.r, vertexColor.g, vertexColor.b, 1.0f);
         }
-    )";//*/
+    )";
 
     // create shader object
     unsigned int vertexShader;
@@ -163,18 +143,18 @@ int Window::SetupOpenGL() {
 
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
-
     // set it to be for vertices
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     // bind Vertex Array Object
     glBindVertexArray(VAO);
 
     // set vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    // color
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
     return SUCCESS;
 }
@@ -187,36 +167,9 @@ std::vector<unsigned int> Window::PollEvent() {
     return events;
 }
 
-#include <array>
-static constexpr auto triangleVertexIndices = std::array{
-    // front
-    0, 1, 2, // first triangle
-    2, 3, 0, // second triangle
-
-    // top
-    4, 5, 6, // first triangle
-    6, 7, 4, // second triangle
-
-    // left
-    8, 9, 10,  // first triangle
-    10, 11, 8, // second triangle
-
-    // right
-    14, 13, 12, // 12, 13, 14, // first triangle
-    12, 15, 14, // 14, 15, 12, // second triangle
-
-    // back
-    18, 17, 16, // 16, 17, 18, // first triangle
-    16, 19, 18, // 18, 19, 16, // second triangle
-
-    // bottom
-    20, 21, 22, // first triangle
-    22, 23, 20  // second triangle
-};
-
 int Window::DrawFrame(Universe* universe) {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     const std::map<long long, Body>* bodies = universe->GetBodies();
 
@@ -226,9 +179,9 @@ int Window::DrawFrame(Universe* universe) {
 
     // test vertex data
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f
+        -0.75f, -0.75f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.75f, -0.75f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f,  0.75f, 0.0f, 0.0f, 0.0f, 1.0f
     };
 
     // copy vertex data into buffer
@@ -236,8 +189,6 @@ int Window::DrawFrame(Universe* universe) {
 
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
-    //glDrawElements(GL_TRIANGLES, 2, GL_UNSIGNED_INT, 0);
-    //glDrawElements(GL_TRIANGLES, triangleVertexIndices.size(), GL_UNSIGNED_INT, 0);
 
     SDL_GL_SwapWindow(_window);
     return SUCCESS;
